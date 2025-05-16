edf5b456ecb61b82675b976e6f8adbec3c2546d6
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-05-17 00:24:14 +0900

    [Bronze II] Title: Adding Reversed Numbers, Time: 264 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Bronze/3486. Adding Reversed Numbers/Adding Reversed Numbers.cc
+++ 백준/Bronze/3486. Adding Reversed Numbers/Adding Reversed Numbers.cc
@@ -3,14 +3,22 @@
 #include <cmath>
 using namespace std;
 
-string ZeroDelete(string str)
+int Reverse(string num)
+{
+    int sum = 0;
+    for (int j=0; j<(int)num.length(); j++) sum += (num[num.length() - j - 1] - '0')
+        * pow(10, (num.length() - j - 1));
+    return sum;
+}
+
+int ZeroDelete(string str)
     {
         while (str[str.length()-1] == '0')
                 {
                     int c = stoi(str) / 10;
                     str = to_string(c);
                 }
-        return str;
+        return Reverse(str);
     }
 
 int main() 
@@ -18,24 +26,12 @@ int main()
     int T; cin >> T;
     for (int i=0; i<T; i++)
         {
-            int first, second; cin >> first >> second;
-            string a, b; 
-            a = to_string(first);
-            b = to_string(second);
+            string a, b; cin >> a >> b;
             
-            string A = ZeroDelete(a);
-            string B = ZeroDelete(b);
+            int A = ZeroDelete(a);
+            int B = ZeroDelete(b);
+            string sum = to_string(A + B);
 
-            int Alpha = 0;
-            int Beta = 0;
-            for (int j=0; j<(int)A.length(); j++) Alpha +=  (A[A.length() - j - 1] - '0') * pow(10, (A.length() - j - 1));
-            for (int k=0; k<(int)B.length(); k++) Beta += (B[B.length() - k - 1] - '0') * pow(10, (B.length() - k - 1));
-            
-            int sum = Alpha + Beta;
-            string C = ZeroDelete(to_string(sum));
-
-            int answer = 0;
-            for (int l=0; l<(int)C.length(); l++) answer += (C[C.length() - l - 1] - '0') * pow(10, (C.length() - l - 1));
-            cout << answer << "\n";
+            cout << ZeroDelete(sum) << "\n";
         }
 }
