69cd2a91048bf737816ddf75d23db1f882378783
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-06-13 13:45:41 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,14 +1,17 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
     int L; cin >> L;
-    int sum = 0;
+    long long sum = 0;
+    long long pow = 1;
+    int M = 1234567891;
     for (int i=0; i<L; i++)
         {
             char ch; cin >> ch;
-            sum += (ch - 'a' + 1) * pow(31, i);
+            int value = ch - 'a' + 1;
+            sum = (sum + (value * pow) % M) % M;
+            pow = (pow * 31) % M;               
         }
-    cout << sum % 1234567891;
+    cout << sum;
 }
