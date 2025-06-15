229eb5c66c49fc5035df330b773532b07c3ec394
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-06-15 18:32:40 +0900

    [level 0] Title: 문자열 섞기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string str1, string str2) {
+    string answer = "";
+    for (int i=0; i < str1.length() * 2; i++)
+    {
+        if (i%2 == 0) answer += str1[i/2];
+        else answer += str2[i/2];
+    }
+    return answer;
+}
