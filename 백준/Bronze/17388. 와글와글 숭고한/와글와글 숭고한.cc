898e1dc5d971cf02aa10b9cd2439710ac582e514
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-06-19 15:03:12 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c; cin >> a >> b >> c;
+    if (a + b + c >= 100) cout << "OK";
+    else if (a < b && a < c) cout << "Soongsil";
+    else if (b < a && b < c) cout << "Korea";
+    else cout << "Hanyang";
+}
