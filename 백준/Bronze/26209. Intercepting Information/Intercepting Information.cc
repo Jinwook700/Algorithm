6ee84b7c009244f1e018c58ba9e39c39d4f42fe9
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-06-30 22:43:31 +0900

    [Bronze V] Title: Intercepting Information, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool flag = true;
+    for (int i=0; i<8; i++)
+        {
+            int num; cin >> num;
+            if (num == 9) flag = false;
+        }
+    if (flag) cout << "S";
+    else cout << "F";
+}
