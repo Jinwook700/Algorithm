49544a3c5c10be41e899c09942ff15a69313be0e
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-08-01 18:00:43 +0900

    [Bronze I] Title: 변수명, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/16205. 변수명/변수명.cc
+++ 백준/Bronze/16205. 변수명/변수명.cc
@@ -2,67 +2,60 @@
 using namespace std;
 
 int main() {
-    int n; cin >> n;
-    string str; cin >> str;
-    for (n = 1; n<4; n++)
-        {
-            if (n == 1 || n == 3)
-    {
-        bool flag = false;
-        bool isFirst = false;
-        bool isFront = false;
-        if (n == 1) isFront = true;
-        if (n == 3) isFirst = true;
-        
-        for (char c : str)
-            {
-                if (isFront)
-                {
-                    char ch = tolower(c);
-                    cout << ch;
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+
+    for (n = 1; n < 4; n++) {
+        if (n == 1 || n == 3) {
+            bool flag = false;
+            bool isFirst = false;
+            bool isFront = false;
+
+            if (n == 1) isFront = true;
+            if (n == 3) isFirst = true;
+
+            for (char c : str) {
+                if (isFront) {
+                    cout << (char)tolower(c);
                     isFront = false;
                 }
-                else if (isFirst)
-                {
-                    char ch = toupper(c);
-                    cout << ch;
+                else if (isFirst) {
+                    cout << (char)toupper(c);
                     isFirst = false;
                 }
-                else if (flag)
-                {
-                    char ch = toupper(c);
-                    cout << ch;
+                else if (flag) {
+                    cout << (char)toupper(c);
                     flag = false;
                 }
-                else if (c == '_')
-                {
+                else if (c == '_') {
                     flag = true;
                     continue;
                 }
-                else cout << c;
+                else {
+                    cout << c;
+                }
             }
-        cout << "\n";
-    }
-        
-    else if (n == 2)
-    {
-        bool isFront = true;
-        for (char c : str)
-            {
-                if (isFront)
-                {
-                    char ch = tolower(c);
-                    cout << ch;
+            cout << "\n";
+        }
+
+        else if (n == 2) {
+            bool isFront = true;
+
+            for (char c : str) {
+                if (isFront) {
+                    cout << (char)tolower(c);
                     isFront = false;
                 }
-                else if (c >= 65 && c <= 90)
-                {
-                    char ch = tolower(c);
-                    cout << '_' << ch;
+                else if (c >= 'A' && c <= 'Z') {
+                    cout << '_' << (char)tolower(c);
                 }
-                else cout << c;
+                else {
+                    cout << c;
+                }
             }
-        cout << "\n";
-    }
+            cout << "\n";
         }
+    }
 }
