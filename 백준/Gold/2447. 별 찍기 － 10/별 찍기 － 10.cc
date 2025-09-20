63f2f6046c8d47e6dd506c2d07b6def1dc5fdeab
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-09-20 22:30:59 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 240 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void star(int i, int j, int num)
+{
+    if (num == 1)
+    {
+        cout << "*";
+    }
+    else if ((i/(num/3))%3 == 1 && (j/(num/3))%3 == 1)
+    {
+        cout << " ";
+    }
+    else
+    {
+        star(i, j, num/3);
+    }
+}
+
+int main()
+{
+    int N; cin >> N;
+    for (int i=0; i<N; i++)
+        {
+            for (int j=0; j<N; j++)
+                {
+                    star(i, j, N);
+                }
+            cout << "\n";
+        }
+}
