0bf520140a1e0580e38c929c71cb2e43397980b7
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-10-23 20:51:24 +0900

    [Silver III] Title: N과 M (2), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void permutation(const vector<int> &v, int m, vector<int>& currentV, vector<bool>& isUsed)
+{
+   if (currentV.size() == m)
+   {
+       vector<int> checkV(currentV.begin(), currentV.end());
+       sort(checkV.begin(), checkV.end());
+       if (currentV != checkV) return;
+       for (int x : currentV) cout << x << " ";
+       cout << "\n";
+       return;
+   }
+
+    for (int i = 0; i < v.size(); i++)
+        {
+            if (isUsed[i]) continue;
+            isUsed[i] = true;
+            currentV.push_back(v[i]);
+
+            permutation(v, m, currentV, isUsed);
+
+            currentV.pop_back();
+            isUsed[i] = false;
+        }
+}
+
+int main() {
+    int n, m; cin >> n >> m;
+    vector<int> v(n, 0);
+    for (int i=0; i<n; i++) v[i] = i+1;
+    vector<int> currentV;
+    vector<bool> isUsed(v.size(), false);
+    permutation(v, m, currentV, isUsed);
+}
