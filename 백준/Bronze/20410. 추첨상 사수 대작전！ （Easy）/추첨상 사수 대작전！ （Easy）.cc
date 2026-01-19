d8f11e9fe5a5fd49f86623c831b79d82dfa95074
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-01-19 17:00:18 +0900

    [Bronze III] Title: 추첨상 사수 대작전! (Easy), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20410. 추첨상 사수 대작전！ （Easy）/추첨상 사수 대작전！ （Easy）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, seed, x1, x2; cin >> m >> seed >> x1 >> x2;
+    for (int a=0; a<m; a++)
+        for (int c=0; c<m; c++)
+                if (x1 == (a*seed + c)%m && x2 == (a*x1 + c)%m)
+                    {
+                        cout << a << " " << c;
+                        exit(0);
+                    }
+}
