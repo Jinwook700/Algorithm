6747e8fa9582c2d762e61a4107b366a6abf3c97e
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-02-04 20:30:08 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 2; i++)
+        {
+            int a, b, c, d, e; cin >> a >> b >> c >> d >> e;
+            cout << a*6 + b*3 + c*2 + d + e*2 << " ";
+        }
+}
