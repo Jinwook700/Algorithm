b110aac2f709226a2605252fb6a3d8554cfaee88
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-03-16 16:58:40 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C ,D; cin >> A >> B >> C >> D;
+    if (A + C > B + D) cout << "Yongdap";
+    else if (A + C < B + D) cout << "Hanyang Univ.";
+    else cout << "Either";
+
+    return 0;
+}
