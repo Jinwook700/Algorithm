4a6196f95d88c2a53d142c248f64fb52c1c4d20c
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-04-13 16:09:51 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string trophy = R"(  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\)";
+    cout << trophy;
+}
