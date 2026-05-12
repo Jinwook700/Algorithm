008b446ba8b68da33a01ee4402e25e4fbe4698ad
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-05-12 16:35:25 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.68 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = 0;
+    for (char c : control)
+    {
+        switch (c)
+        {
+            case 'w':
+                n += 1;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+            case 'a':
+                n -= 10;
+                break;
+        }
+    }
+    return n;
+}
