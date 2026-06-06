508d37120609b9c7473da8617163189aa8bb9f54
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-06-06 17:24:07 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    while(true)
+    {
+        if (answer * 6 % n == 0) break;
+        answer++;
+    }
+    return answer;
+}
