5f1113e0ab593712d4df2a39ca3d429d7764011c
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-07-24 14:57:31 +0900

    [level 2] Title: 짝지어 제거하기, Time: 3.70 ms, Memory: 6.55 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+    stack<char> st;
+    for (char c : s)
+    {
+        if (st.empty() || st.top() != c) st.push(c);
+        else st.pop();
+    }
+    answer = st.empty() ? 1 : 0;
+    return answer;
+}
